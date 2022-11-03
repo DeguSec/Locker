b95e2c400424b0c977327464237ce07b452c3d50
Rijndael <rijndael@baldy.ga>
2022-11-03 17:34:37 +0000

    fix: lint

--- src/preload/crypto-utils/Account.ts
+++ src/preload/crypto-utils/Account.ts
@@ -1,31 +1,37 @@
 import { Extra } from "./Extra/Extra.js";
 
 /**
- * This class stores all of the information for each account. 
+ * This class stores all of the information for each account.
  */
 export class Account implements iJSON {
-  public website: string;
-  public password: string;
-  public login: string;
-  public extra: Extra; // Extra data added by user
-  constructor(accountData?: any) {
-    if (accountData == null) {
-      this.website = this.password = this.login = "";
-      this.extra = new Extra();
-    } else {
-      this.website = accountData["website"];
-      this.password = accountData["password"];
-      this.login = accountData["login"];
-      this.extra = new Extra(JSON.parse(accountData["extra"]));
-    }
-  }
+	public website: string;
 
-  getJSON() {
-    return JSON.stringify({
-      "website": this.website,
-      "password": this.password,
-      "login": this.login,
-      "extra": this.extra.getJSON(),
-    });
-  }
+	public password: string;
+
+	public login: string;
+
+	public extra: Extra; // Extra data added by user
+
+	constructor(accountData?: any) {
+		if (accountData == null) {
+			this.website = "";
+			this.password = "";
+			this.login = "";
+			this.extra = new Extra();
+		} else {
+			this.website = accountData.website;
+			this.password = accountData.password;
+			this.login = accountData.login;
+			this.extra = new Extra(JSON.parse(accountData.extra));
+		}
+	}
+
+	getJSON() {
+		return JSON.stringify({
+			"website": this.website,
+			"password": this.password,
+			"login": this.login,
+			"extra": this.extra.getJSON(),
+		});
+	}
 }
