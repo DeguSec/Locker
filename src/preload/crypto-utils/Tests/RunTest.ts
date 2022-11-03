161d62b38438548e390d8a7d7b3ec0e114b9f017
Rijndael <rijndael@baldy.ga>
2022-11-03 16:58:59 +0000

    fix: run

--- src/preload/crypto-utils/Tests/RunTest.ts
+++ src/preload/crypto-utils/Tests/RunTest.ts
@@ -1,45 +1,40 @@
-let check = "✅"; //✔️
-let cross = "❎"; //❌
+const check = "✅"; // ✔️
+const cross = "❎"; // ❌
 
-export class Result {
-    test_result: string;
-    function_name: string;
-    constructor(test_result: string, function_name: string) {
-        this.test_result = test_result;
-        this.function_name = function_name;
-    }
-}
+
 
 export class RunTest {
-    tests = [] as (() => Promise<boolean>)[];
+	tests = [] as Array<(() => Promise<boolean>)>;
 
-    RunTests(resultObject : HTMLElement) {
-        for(let test = 0; test < this.tests.length; test++) {
-            new Promise(() => {
-                let currentTest = this.tests[test];
+	// TODO: Run tests here
+	// eslint-disable-next-line class-methods-use-this
+	RunTests(resultObject : HTMLElement) {
+		// for(let test = 0; test < this.tests.length; test++) {
+		// 	new Promise(() => {
+		// 		const currentTest = this.tests[test];
 
-                // create objects
-                let domObject = document.createElement("div");
-                let testName = document.createElement("div");
-                let testResult = document.createElement("div");
-                domObject.appendChild(testResult);
-                domObject.appendChild(testName);
-                resultObject.appendChild(domObject);
-    
-                // assign values
-                domObject.classList.add("test");
-                testName.classList.add("test-title");
-                testResult.classList.add("test-result");
-    
-                testName.textContent = currentTest.name;
-                testResult.textContent = "Waiting...";
-    
-                currentTest().then((res) => {
-                    testResult.textContent = res ? check : cross; 
-                }, (reason) => {
-                    testResult.textContent = cross + " " + reason;
-                });
-            });
-        }
-    }
+		// 		// create objects
+		// 		const domObject = document.createElement("div");
+		// 		const testName = document.createElement("div");
+		// 		const testResult = document.createElement("div");
+		// 		domObject.appendChild(testResult);
+		// 		domObject.appendChild(testName);
+		// 		resultObject.appendChild(domObject);
+
+		// 		// assign values
+		// 		domObject.classList.add("test");
+		// 		testName.classList.add("test-title");
+		// 		testResult.classList.add("test-result");
+
+		// 		testName.textContent = currentTest.name;
+		// 		testResult.textContent = "Waiting...";
+
+		// 		currentTest().then((res) => {
+		// 			testResult.textContent = res ? check : cross;
+		// 		}, (reason) => {
+		// 			testResult.textContent = `${cross  } ${  reason}`;
+		// 		});
+		// 	});
+		// }
+	}
 }
