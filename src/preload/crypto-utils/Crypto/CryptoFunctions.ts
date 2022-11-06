c5a6320d3e9eafc20206e4d8e7254cac56c60d62
Rijndael <rijndael@baldy.ga>
2022-11-06 17:46:42 +0000

    fix: lint

--- src/preload/crypto-utils/Crypto/CryptoFunctions.ts
+++ src/preload/crypto-utils/Crypto/CryptoFunctions.ts
@@ -1,3 +1,11 @@
+/* eslint-disable new-cap */
+// TODO: Fix new-cap
+
+import Crypto from "crypto";
+
+import aesjs from 'aes-js';
+import { hash as Argon2, argon2id, Options as ArgonOptions } from "argon2";
+
 import { EncryptionType, KeyDerivationFunction } from "../CustomTypes.js";
 import { PasswordSettings } from "../Extra/Settings/PasswordSettings.js";
 import { Settings } from "../Extra/Settings/Settings.js";
@@ -9,7 +17,7 @@ import { Settings } from "../Extra/Settings/Settings.js";
  * @returns Returns the correct length of bytes a key should be for different algorithms
  */
 export function algorithmBytes(algorithm: EncryptionType) {
-	return algorithm != "Blow" ? 32 : 56;
+	return algorithm !== "Blow" ? 32 : 56;
 }
 
 /**
@@ -18,8 +26,8 @@ export function algorithmBytes(algorithm: EncryptionType) {
  * @param algorithm algorithm for iv bytes
  * @returns Returns the correct IV length in bytes for the different algorithms
  */
-export function algorithmIvBytes(algorithm : EncryptionType) {
-	return algorithm == "Blow" ? 8 : 16;
+export function algorithmIvBytes(algorithm: EncryptionType) {
+	return algorithm === "Blow" ? 8 : 16;
 }
 
 
@@ -33,8 +41,8 @@ export function algorithmIvBytes(algorithm : EncryptionType) {
  * @returns Argon2 hash as {@link Uint8Array}
  */
 export async function hashArgon2(memory: number, iterations: number, salt: any, keySize: number, password: string) {
-	const optionals = {
-		type: Argon2.argon2id,
+	const optionals: ArgonOptions = {
+		type: argon2id,
 		memoryCost: memory,
 		timeCost: iterations,
 		salt,
@@ -42,23 +50,37 @@ export async function hashArgon2(memory: number, iterations: number, salt: any, 
 		raw: true,
 	}
 
+	// TODO: FIX
 	return await Argon2(password, optionals) as Uint8Array;
 }
 
-const Crypto = require("crypto");
+/**
+ * Generates random bytes
+ * @param length how many bytes to generate
+ * @returns an array of random bytes the size specified.
+ */
+export function getRandomBytes(length: number): Uint8Array {
+	return Crypto.randomBytes(length) as Uint8Array;
+}
+
 
 /**
- * Function hashing using PBKDF2-SHA512. {@link https://nodejs.org/api/crypto.html#crypto_crypto_pbkdf2_password_salt_iterations_keylen_digest_callback}
+ * Function hashing using PBKDF2-SHA512.
+ * {@link https://nodejs.org/api/crypto.html#crypto_crypto_pbkdf2_password_salt_iterations_keylen_digest_callback}
  * @param iterations time costs
  * @param salt salt
  * @param keySize key size to return
  * @param password password to hash
- * @returns a PBKDF2-SHA512 hash. 
+ * @returns a PBKDF2-SHA512 hash.
  */
-export async function hashPBKDF2(iterations: number, salt: string | ArrayBuffer | Buffer | DataView, keySize: number, password: string | ArrayBuffer | Buffer | DataView): Promise<Uint8Array> {
+export async function hashPBKDF2(
+	iterations: number,
+	salt: Crypto.BinaryLike,
+	keySize: number,
+	password: Crypto.BinaryLike): Promise<Uint8Array> {
 	iterations = iterations > 4294967296 ? 4294967295 : iterations; // keep to the limit
 	return new Promise((res, rej) => {
-		// This is stupid. Please fix node. 
+		// This is stupid. Please fix node.
 		// https://nodejs.org/api/crypto.html#crypto_crypto_pbkdf2sync_password_salt_iterations_keylen_digest
 		// https://stackoverflow.com/questions/49717731/error-no-callback-provided-to-pbkdf2-when-using-async-await#54032711
 		Crypto.pbkdf2(password, salt, iterations, keySize, "sha512", (err: any, key: Buffer) => err ? rej(err) : res(Uint8Array.from(key)));
@@ -67,20 +89,20 @@ export async function hashPBKDF2(iterations: number, salt: string | ArrayBuffer 
 
 /**
  * Generates a password given the settings
- * @param The {@link PasswordSettings password settings} that will be used to generate the password. 
- * @returns a password that follows the rules in the {@link PasswordSettings password settings}. If a password cannot be generated, "" is returned. 
+ * @param The {@link PasswordSettings password settings} that will be used to generate the password.
+ * @returns a password that follows the rules in the {@link PasswordSettings password settings}. If a password cannot be generated, "" is returned.
  */
-export function generatePassword(passwordSettings ?: PasswordSettings) {
+export function generatePassword(passwordSettings?: PasswordSettings) {
 	passwordSettings = passwordSettings == null ? new Settings().passwordSettings : passwordSettings;
 	let charactarPool = "";
 	charactarPool += passwordSettings.includeLowercase ? passwordSettings.lowercase : "";
 	charactarPool += passwordSettings.includeUppercase ? passwordSettings.uppercase : "";
 	charactarPool += passwordSettings.includeNumbers ? passwordSettings.numbers : "";
 	charactarPool += passwordSettings.includeSymbols ? passwordSettings.symbols : "";
-	if(charactarPool == "") return "";
+	if (charactarPool === "") return "";
 
-	let password = ""; 
-	for(let i = 0; i < passwordSettings.passwordLength; i++) {
+	let password = "";
+	for (let i = 0; i < passwordSettings.passwordLength; i++) {
 		// Using crypto for security
 		password += charactarPool[Crypto.randomInt(charactarPool.length)];
 	}
@@ -88,18 +110,18 @@ export function generatePassword(passwordSettings ?: PasswordSettings) {
 	return password;
 }
 
-const aesjs = require('aes-js');
-const Argon2 = require("argon2").hash;
+
+
 
 /**
- * Encrypt data with key and iv. This function also padds the data to make it fit within 16 bytes. 
+ * Encrypt data with key and iv. This function also padds the data to make it fit within 16 bytes.
  * @param key key to encrypt with
  * @param iv iv to encrypt with
  * @param data data to encrypt
  * @returns encrypted data
  */
 export function encryptAES(key: Uint8Array, iv: Uint8Array, data: Uint8Array) {
-	if (data.length == 0) throw "Data is empty...";
+	if (data.length === 0) throw new Error("Data is empty...");
 
 	// add padding to data (at least 16 is added for the sake of consistancy)
 	const paddingRequired = 16 + (16 - (data.length % 16)) - 1;
@@ -124,7 +146,7 @@ export function encryptAES(key: Uint8Array, iv: Uint8Array, data: Uint8Array) {
 }
 
 /**
- * Decrypt encrypted data with key and iv. This function also removes the pad added to the data to make it fit within 16 bytes. 
+ * Decrypt encrypted data with key and iv. This function also removes the pad added to the data to make it fit within 16 bytes.
  * @param key key to decrypt with
  * @param iv iv to decrypt with
  * @param data data to decrypt
@@ -169,29 +191,28 @@ export function decryptBlowfish(key: Uint8Array, iv: Uint8Array, encryptedData: 
 }
 
 /**
- * Generates random bytes
- * @param length how many bytes to generate
- * @returns an array of random bytes the size specified.  
- */
-export function getRandomBytes(length: number): Uint8Array {
-	return Crypto.randomBytes(length) as Uint8Array;
-}
-
-/**
  * Get the hash given the function and settings
  * @param keyDerivationFunction the hash function to use
  * @param rounds time costs
  * @param salt salt
- * @param keyByteSize bytes to return 
+ * @param keyByteSize bytes to return
  * @param password string to hash, usually a password
  * @param roundsMemory memory costs (Argon2 only)
- * @returns hashed represetation of the password parameter given the settings. 
+ * @returns hashed represetation of the password parameter given the settings.
  */
-export async function getKeyHash(keyDerivationFunction: KeyDerivationFunction, rounds: number, salt: Uint8Array, keyByteSize: number, password: string, roundsMemory: number | null) {
+export async function getKeyHash(
+	keyDerivationFunction: KeyDerivationFunction,
+	rounds: number,
+	salt: Uint8Array,
+	keyByteSize: number,
+	password: string,
+	roundsMemory: number | null) {
 	let key: Uint8Array;
 	switch (keyDerivationFunction) {
 		case "Argon2":
-			if (roundsMemory == null) throw "Argon2 NEEDS 'roundsMemory'. roundsMemory is null";
+			if (roundsMemory == null)
+				throw new Error("Argon2 NEEDS 'roundsMemory'. roundsMemory is null");
+
 			key = await hashArgon2(roundsMemory, rounds, salt, keyByteSize, password);
 			break;
 
@@ -200,14 +221,14 @@ export async function getKeyHash(keyDerivationFunction: KeyDerivationFunction, r
 			break;
 
 		default:
-			throw `${keyDerivationFunction  } is not a supported derivation function`;
+			throw new Error(`${keyDerivationFunction} is not a supported derivation function`);
 	}
 	return key;
 }
 
 /**
  * Encrypt data
- * @param encryptionType the encrypton type to use 
+ * @param encryptionType the encrypton type to use
  * @param key the key to use
  * @param iv the iv to use
  * @param data the data to encrypt
@@ -225,7 +246,7 @@ export function encrypt(encryptionType: EncryptionType, key: Uint8Array, iv: Uin
 			break;
 
 		default:
-			throw `${encryptionType  } is not a supported encryption type`;
+			throw new Error(`${encryptionType} is not a supported encryption type`);
 	}
 
 	return encryptedData;
@@ -251,7 +272,7 @@ export function decrypt(encryptionType: EncryptionType, key: Uint8Array, iv: Uin
 			break;
 
 		default:
-			throw `${encryptionType  } is not a supported encryption type`;
+			throw new Error(`${encryptionType} is not a supported encryption type`);
 	}
 
 	return decryptedData;
@@ -263,7 +284,7 @@ export function decrypt(encryptionType: EncryptionType, key: Uint8Array, iv: Uin
  * @returns hashed representation
  */
 export function hash(data: Uint8Array) {
-	const hashElement = Crypto.createHash("sha512", data);
+	const hashElement = Crypto.createHash("sha512");
 	hashElement.update(data);
 	return Uint8Array.from(hashElement.digest());
 }
